7ae587ccce5f565f6ac587f74e890aea42673d0b
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-01 18:48:29 +0000

    Add project files.

---
+++ ClimateControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IW_ClimateControl
+{
+    internal class ClimateControl
+    {
+    }
+}
