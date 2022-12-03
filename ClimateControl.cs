fa6934748ff4e92581ad09b3c63d09d0dc4c6d22
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-03 03:49:18 +0000

    Implemented first version of custom weather system

--- ClimateControl.cs
+++ ClimateControl.cs
@@ -18,6 +18,8 @@ namespace IWClimateControl
         Tuple<string, string> GetWeatherInfo();
         string TranslateTomorrowStates(int integerState);
         int TranslateTomorrowStates(string stringState);
+        double RollTheDice();
+        int RollTheDiceInt();
     }
 
     internal sealed class ClimateControl : Mod
@@ -30,9 +32,9 @@ namespace IWClimateControl
         // Main method
         public override void Entry(IModHelper helper)
         {
-            // Tell SMAPI to grab config values and/or make config.json if absent
+            // Tell SMAPI where to grab config values and to make config.json if absent
             this.Config = this.Helper.ReadConfig<CCConfig>();
-            // Populate class config fields
+            // Grab weather chances for calculations
             weatherChances = GrabWeatherChances();
 
             // When day begins, set tomorrow's weather
@@ -43,54 +45,25 @@ namespace IWClimateControl
         // Change tomorrow's weather
         private void DayStarted_ChangeWeather(object sender, DayStartedEventArgs e)
         {
-            // Grab weather chances for calculations
-            this.Monitor.Log($"The Spring Rain chance is {weatherChances["Spring"]["Rain"]}", LogLevel.Info);
-
-            // Grab from IWAPI
+            // Grab relevant info for calculation
+            var currentDate = Game1.Date;
             IIWAPI api = this.Helper.ModRegistry.GetApi<IIWAPI>("MsBontle.ImmersiveWeathers");
-            if (api != null)
-            {
-                //Tuple<string, string> weatherInfo = api.GetWeatherInfo();
-                //int tomorrowInteger = api.TranslateTomorrowStates(weatherInfo.Item2);
-                //string tomorrowString = api.TranslateTomorrowStates(tomorrowInteger);
-                //this.Monitor.Log($"Tomorrow's weather is {weatherInfo.Item2}, which corresponds to an integer of {tomorrowInteger} and a string of {tomorrowString}", LogLevel.Info);
-                //this.Monitor.Log($"Today's day is {Game1.Date.DayOfWeek}", LogLevel.Info);
 
-                //string currentDay = Game1.Date.DayOfWeek.ToString();
-                
-                /*
-                if (currentDay == "Monday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Sunny");
-                }
-                else if (currentDay == "Tuesday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Raining");
-                }
-                else if (currentDay == "Wednesday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Snowing");
-                }
-                else if (currentDay == "Thursday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Windy");
-                }
-                else if (currentDay == "Friday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Wedding");
-                }
-                else if (currentDay == "Saturday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Festival");
-                }
-                else if (currentDay == "Sunday")
-                {
-                    Game1.weatherForTomorrow = api.TranslateTomorrowStates("Storming");
-                }
-                */
+            // Check if tomorrow is OK to change
+            if ((currentDate.DayOfMonth != 28) &&
+                (Game1.weatherForTomorrow != api.TranslateTomorrowStates("Festival")) &&
+                (Game1.weatherForTomorrow != api.TranslateTomorrowStates("Wedding")))
+            {
+                // If so, attempt to change it
+                string weatherJackpot = WeatherSlotMachine.GenerateWeather(currentDate, weatherChances, api);
+                this.Monitor.Log($"Weather for tomorrow changed to {weatherJackpot}", LogLevel.Info);
+                Game1.weatherForTomorrow = api.TranslateTomorrowStates(weatherJackpot);
             }
-            //Game1.weatherForTomorrow = 1;
-            
+            else
+            {
+                // If not, don't touch it
+                this.Monitor.Log("Tomorrow will always be sunny, so no changes will be made.", LogLevel.Info);
+            }  
         }
         
         // Grab weather chances from config
@@ -99,43 +72,43 @@ namespace IWClimateControl
             return new Dictionary<string, Dictionary<string, double>>
             {
                 {
-                    "Spring",
+                    "spring",
                     new Dictionary<string, double>
                     {
-                        { "Rain", this.Config.SpringRainChance },
-                        { "Storm", this.Config.SpringStormChance },
-                        { "Wind", this.Config.SpringWindChance },
-                        { "Snow", this.Config.SpringSnowChance }
+                        { "rain", this.Config.SpringRainChance },
+                        { "storm", this.Config.SpringStormChance },
+                        { "wind", this.Config.SpringWindChance },
+                        { "snow", this.Config.SpringSnowChance }
                     }
                 },
                 {
-                    "Summer",
+                    "summer",
                     new Dictionary<string, double>
                     {
-                        { "Rain", this.Config.SummerRainChance },
-                        { "Storm", this.Config.SummerStormChance },
-                        { "Wind", this.Config.SummerWindChance },
-                        { "Snow", this.Config.SummerSnowChance }
+                        { "rain", this.Config.SummerRainChance },
+                        { "storm", this.Config.SummerStormChance },
+                        { "wind", this.Config.SummerWindChance },
+                        { "snow", this.Config.SummerSnowChance }
                     }
                 },
                 {
-                    "Fall",
+                    "fall",
                     new Dictionary<string, double>
                     {
-                        { "Rain", this.Config.FallRainChance },
-                        { "Storm", this.Config.FallStormChance },
-                        { "Wind", this.Config.FallWindChance },
-                        { "Snow", this.Config.FallSnowChance }
+                        { "rain", this.Config.FallRainChance },
+                        { "storm", this.Config.FallStormChance },
+                        { "wind", this.Config.FallWindChance },
+                        { "snow", this.Config.FallSnowChance }
                     }
                 },
                 {
-                    "Winter",
+                    "winter",
                     new Dictionary<string, double>
                     {
-                        { "Rain", this.Config.WinterRainChance },
-                        { "Storm", this.Config.WinterStormChance },
-                        { "Wind", this.Config.WinterWindChance },
-                        { "Snow", this.Config.WinterSnowChance }
+                        { "rain", this.Config.WinterRainChance },
+                        { "storm", this.Config.WinterStormChance },
+                        { "wind", this.Config.WinterWindChance },
+                        { "snow", this.Config.WinterSnowChance }
                     }
                 }
             };
---
+++ WeatherSlotMachine.cs
@@ -0,0 +1,82 @@
+﻿using IWClimateControl;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IW_ClimateControl
+{
+    // Handles weather outcomes
+    internal class WeatherSlotMachine
+    {
+        // Generate weather based on config values
+        public static string GenerateWeather(WorldDate currentDate, Dictionary<string, Dictionary<string, double>> weatherChances, IIWAPI api)
+        {
+            string weatherJackpot = "";
+            string currentSeason = currentDate.Season;
+            // Flip a coin for each state. All at once so custom priorities can be implemented later
+            bool stormBool = WeatherCoin.FlipStormCoin(weatherChances[currentSeason]["storm"], api);
+            bool rainBool = WeatherCoin.FlipRainCoin(weatherChances[currentSeason]["rain"], api);
+            bool snowBool = WeatherCoin.FlipSnowCoin(weatherChances[currentSeason]["snow"], api);
+            bool windBool = WeatherCoin.FlipWindCoin(weatherChances[currentSeason]["wind"], api);
+            // For now, prioritise storms over rain over wind over snow
+            if (stormBool)
+                weatherJackpot = "Storming";
+            else if (rainBool)
+                weatherJackpot = "Raining";
+            else if (windBool)
+                weatherJackpot = "Windy";
+            else if (snowBool)
+                weatherJackpot = "Snowing";
+            else
+                weatherJackpot = "Sunny";
+            return weatherJackpot;
+        }
+
+    }
+
+    // Flips coins for weather states
+    internal class WeatherCoin
+    {
+        // Thunderstorm
+        public static bool FlipStormCoin(double stormChance, IIWAPI api)
+        {
+            // If dice roll lands within the percentage, permit the change.
+            // Otherwise, deny it. Smaller percentages have narrower
+            // windows, corresponding to a lower likelihood of change.
+            bool stormBool = false;
+            if ((0.01 * stormChance) >= api.RollTheDice())
+                stormBool = true;
+            return stormBool;
+        }
+
+        // Rain
+        public static bool FlipRainCoin(double rainChance, IIWAPI api)
+        {
+            bool rainBool = false;
+            if ((0.01 * rainChance) >= api.RollTheDice())
+                rainBool = true;
+            return rainBool;
+        }
+
+        // Snow
+        public static bool FlipSnowCoin(double snowChance, IIWAPI api)
+        {
+            bool snowBool = false;
+            if ((0.01 * snowChance) >= api.RollTheDice())
+                snowBool = true;
+            return snowBool;
+        }
+
+        // Wind
+        public static bool FlipWindCoin(double windChance, IIWAPI api)
+        {
+            bool windBool = false;
+            if ((0.01 * windChance) >= api.RollTheDice())
+                windBool = true;
+            return windBool;
+        }
+    }
+}
