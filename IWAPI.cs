f34aefdd4422b2be60b0b1efce81fecbb755e562
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-05 03:37:34 +0000

    - Added integration with Framework. Now broadcasts weather changes to SMAPI console through the Framework. - Created a class framework for storing multiple different template models and printing these to json files at first launch. Major code re-write to accomodate this. - If player edits a template's json files, mod will use the edited values. - Player can also create a custom template by editing config.json and also specifying a "custom" model. - Subcategorised seasonal weather into "Mid" grouping, in anticipation of "Early", "Mid" and "Late" chances of each weather type. - Fleshed out code comments. - Added list of models to API. - Made sure mod only caches Framework's API once rather than daily. - Mod only checks for model choice the first time.

---
+++ IWAPI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IW_ClimateControl
+{
+    // Internal interface for IWAPI
+    public interface IWAPI
+    {
+        public enum WeatherType
+        {
+            sunny = 0,
+            raining = 1,
+            windy = 2,
+            storming = 3,
+            festival = 4,
+            snowing = 5,
+            wedding = 6,
+            unknown = 7
+        }
+        public enum SeasonType
+        {
+            spring = 0,
+            summer = 1,
+            fall = 2,
+            winter = 3
+        }
+        Tuple<string, string> GetWeatherInfo();
+        string TranslateTomorrowStates(int integerState);
+        int TranslateTomorrowStates(string stringState);
+        double RollTheDice();
+        int RollTheDiceInt();
+        public enum FollowTheWhiteRabbit
+        {
+            ClimateControl = 0
+        }
+        public void WakeUpNeo_TheyreWatchingYou(string messageForNeo, int thisIsMyName);
+        public enum WeatherModel
+        {
+            custom = 0,
+            standard = 1
+        }
+    }
+}
---
+++ Models.cs
@@ -0,0 +1,83 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+using StardewValley.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IW_ClimateControl
+{
+    public class StandardModel
+    {
+        public ModelDefinition Model { get; set; }
+
+        public StandardModel()
+        {
+            Model = new ModelDefinition();
+
+            // very wet but light intensity, vanishing chance of snow
+            Model.Spring.Rain.Mid = 40;
+            Model.Spring.Storm.Mid = 15;
+            Model.Spring.Wind.Mid = 20;
+            Model.Spring.Snow.Mid = 1;
+
+            // mostly sunny, but can be intense rain
+            Model.Summer.Rain.Mid = 10;
+            Model.Summer.Storm.Mid = 20;
+            Model.Summer.Wind.Mid = 5;
+            Model.Summer.Snow.Mid = 0;
+
+            // very dry and windy, small chance of snow
+            Model.Fall.Rain.Mid = 8;
+            Model.Fall.Storm.Mid = 4;
+            Model.Fall.Wind.Mid = 40;
+            Model.Fall.Snow.Mid = 2;
+
+            // lots of snow, seldom rain
+            Model.Winter.Rain.Mid = 5;
+            Model.Winter.Storm.Mid = 0;
+            Model.Winter.Wind.Mid = 20;
+            Model.Winter.Snow.Mid = 40;
+        }
+    }
+
+    public class ModelDefinition
+    {
+        public Season Spring { get; set; }
+        public Season Summer { get; set; }
+        public Season Fall { get; set; }
+        public Season Winter { get; set; }
+
+        public ModelDefinition()
+        {
+            Spring = new Season();
+            Summer = new Season();
+            Fall = new Season();
+            Winter = new Season();
+        }
+    }
+
+    public class Season
+    {
+        public Weather Rain { get; set; }
+        public Weather Storm { get; set; }
+        public Weather Wind { get; set; }
+        public Weather Snow { get; set; }
+
+        public Season()
+        {
+            Rain = new Weather();
+            Storm = new Weather();
+            Wind = new Weather();
+            Snow = new Weather();
+        }
+    }
+
+    public class Weather
+    {
+        public double Mid { get; set; }
+    }
+}
