3b9d74754249c33875cbb2a86850b587f395a01e
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-13 20:27:22 +0000

    feat: reworked config to be compatible GMCM
    Config now has methods for saving, loading and resetting models. This
    allows customised responses to GMCM button-clicks.

--- Config.cs
+++ Config.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Force.DeepCloner;
+using IWClimateControl;
+using ServiceStack.Text;
+using StardewModdingAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IW_ClimateControl
@@ -23,13 +28,65 @@ namespace IW_ClimateControl
         /// </summary>
         public ModConfig()
         {
+            LoadModel(this);
+            ClimateControl.modelChoice = (IWAPI.WeatherModel)Enum.Parse(typeof(IWAPI.WeatherModel), this.ModelChoice);
+        }
+
+        public static void LoadModel(ModConfig Config)
+        {
             // Copy across properties
-            if (this.ModelChoice == "standard")
+            if (Config.ModelChoice == IWAPI.WeatherModel.standard.ToString())
             {
                 // The standard model for generic climates.
-                StandardModel standardModel = new();
-                PropertyMatcher<StandardModel, ModConfig>.GenerateMatchedObject(standardModel, this);
+                PropertyMatcher<StandardModel, ModConfig>.GenerateMatchedObject(ClimateControl.standardModel, Config);
+                ClimateControl.standardModel = ClimateControl.standardModel.DeepClone();
             }
+            else if (Config.ModelChoice == IWAPI.WeatherModel.custom.ToString())
+            {
+                // The custom model, with standard model as the base class.
+                PropertyMatcher<StandardModel, ModConfig>.GenerateMatchedObject(ClimateControl.customModel, Config);
+                ClimateControl.customModel = ClimateControl.customModel.DeepClone();
+            }
+        }
+
+        public static void ResetModel(ModConfig Config, IModHelper Helper)
+        {
+            ClimateControl.eventLogger.SendToSMAPI("I was asked to reset all models", EventType.info);
+            // Reset all models, except custom
+            ClimateControl.standardModel = new();
+            Helper.Data.WriteJsonFile("models/standard.json", ClimateControl.standardModel);
+            //ClimateControl.customModel = new();
+            //Helper.Data.WriteJsonFile("models/standard.json", ClimateControl.customModel);
+
+            // Copy reset values into Config
+            Config.ModelChoice = "standard";
+            ClimateControl.modelChoice = (IWAPI.WeatherModel)Enum.Parse(typeof(IWAPI.WeatherModel), Config.ModelChoice);
+            LoadModel(Config);
+            Helper.WriteConfig(Config);
+        }
+
+        public static void ChangeModel(ModConfig Config, IModHelper Helper)
+        {
+            ClimateControl.eventLogger.SendToSMAPI("I was asked to refresh all models", EventType.info);
+            // Refresh relevant models
+            ClimateControl.eventLogger.SendToSMAPI($"Model was changed from {ClimateControl.modelChoice} to {Config.ModelChoice}. Changes will be applied to {ClimateControl.modelChoice}", EventType.info);
+            // Save changes to old model.
+            if (ClimateControl.modelChoice == IWAPI.WeatherModel.standard)
+            {
+                PropertyMatcher<ModConfig, StandardModel>.GenerateMatchedObject(Config, ClimateControl.standardModel);
+                Helper.Data.WriteJsonFile("models/standard.json", ClimateControl.standardModel);
+                ClimateControl.standardModel = Helper.Data.ReadJsonFile<StandardModel>("models/standard.json");
+            }
+            else if (ClimateControl.modelChoice == IWAPI.WeatherModel.custom)
+            {
+                PropertyMatcher<ModConfig, StandardModel>.GenerateMatchedObject(Config, ClimateControl.customModel);
+                Helper.Data.WriteJsonFile("models/custom.json", ClimateControl.customModel);
+                ClimateControl.customModel = Helper.Data.ReadJsonFile<StandardModel>("models/custom.json");
+            }
+            // Load new model.
+            LoadModel(Config);
+            Helper.WriteConfig(Config);
+            ClimateControl.modelChoice = (IWAPI.WeatherModel)Enum.Parse(typeof(IWAPI.WeatherModel), Config.ModelChoice);
         }
     }
 
